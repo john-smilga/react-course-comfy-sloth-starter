6d11a89a976576e88c79ed067f70a682d92f0fe2
john-smilga <johnsmilga@gmail.com>
2021-01-13 20:26:44 -0800

    fix audit and stripe checkout component added

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
-import { PageHero } from '../components'
+import { PageHero, StripeCheckout } from '../components'
+// extra imports
+import { useCartContext } from '../context/cart_context'
+import { Link } from 'react-router-dom'
+
 const CheckoutPage = () => {
   return <h4>checkout page</h4>
 }
