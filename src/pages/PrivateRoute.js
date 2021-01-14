37651a5598fe43d1c5dafd76c15c0e8d61bbe6a3
John <johnsmilga@gmail.com>
2021-01-14 08:56:18 -0800

    useAuth added to Private Route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
-import { useUserContext } from '../context/user_context'
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+// will remove later
+import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = () => {
-  return <h4>Private Route</h4>
-}
-export default PrivateRoute
+  return <h4>Private Route</h4>;
+};
+export default PrivateRoute;
