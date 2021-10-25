57865117117e817b44c47dc9d7b72adb5cc08270
Jermaine-rbg <jermainebailey@Jermaines-MacBook-Air.local>
2021-10-25 00:42:48 -0400

    Env Keys

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -13,14 +13,133 @@ import { useUserContext } from '../context/user_context'
 import { formatPrice } from '../utils/helpers'
 import { useHistory } from 'react-router-dom'
 
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+
+
+
 const CheckoutForm = () => {
-  return <h4>hello from Stripe Checkout </h4>
+  const { cart, total_amount, shipping_fee, clearCart } = useUserContext();
+  const { myUser } = useUserContext();
+  const history = useHistory();
+  const [succeeded, setSucceeded] = useState(false);
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState('');
+  const stripe = useStripe();
+  const elements = useElements();
+  const cardStyle = {
+    style: {
+      base: {
+        color: '#32325d',
+        fontFamily: 'Arial, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+          color: '#32325d',
+        },
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a',
+      },
+    },
+  }
+
+  const createPaymentIntent = async() => {
+    try {
+      const { data } = await axios.post(
+        '/.netlify/functions/create-payment-intent',
+
+        JSON.stringify({ cart, shipping_fee, total_amount })
+        
+        
+      )
+      
+      setClientSecret(data.clientSecret)
+    } catch (error) {
+      // console.log(error.response)
+    }
+  }
+
+  useEffect(() => {
+    createPaymentIntent();
+    // eslint-disable-next-line
+  }, [])
+
+  const handleChange = async (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty)
+    setError(event.error ? event.error.message : '')
+  }
+
+  const handleSubmit = async (ev) => {
+    ev.preventDefault()
+    setProcessing(true)
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+      },
+    })
+    if(payload.error){
+      setError(`Payment failed ${payload.error.message}`)
+      setProcessing(false)
+    }
+    else{
+      setError(null);
+      setProcessing(false)
+      setSucceeded(true)
+    }
+  }
+  return <div>
+    {succeeded ? (
+        <article>
+          <h4>Thank you</h4>
+          <h4>Your payment was successful!</h4>
+          <h4>Redirecting to home page shortly</h4>
+        </article>
+      ) : (
+        <article>
+          <h4>Hello, {myUser && myUser.name}</h4>
+          <p>Your total is {formatPrice(shipping_fee + total_amount)}</p>
+          <p>Test Card Number: 4242 4242 4242 4242</p>
+        </article>
+      )}
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <CardElement id="card-element" options={cardStyle} onChange={handleChange}/>
+      <button disabled={processing || disabled || succeeded} id="submit">
+        <span id="button-text">
+          {processing ? <div className="spinner" id="spinner">
+
+          </div> : 'Pay'}
+        </span>
+      </button>
+      {/* Show any error that happens when processing the payment */}
+      {error && (
+          <div className='card-error' role='alert'>
+            {error}
+          </div>
+        ) }
+        {/* Show a success message upon completion */}
+        <p className={succeeded ? 'result-message' : 'result-message hidden'}>
+          Payment succeeded, see the result in your
+          <a href={`https://dashboard.stripe.com/test/payments`}>
+            
+            Stripe dashboard.
+          </a>
+          Refresh the page to pay again.
+        </p>
+    </form>
+  </div>
 }
 
 const StripeCheckout = () => {
   return (
     <Wrapper>
+      <Elements stripe={promise}>
       <CheckoutForm />
+      </Elements>
     </Wrapper>
   )
 }
--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import { Auth0Provider } from '@auth0/auth0-react'
 // YuzmhPxxiiv5WxGH7AXqDCnTV2ZPnhtN
 ReactDOM.render(
 <Auth0Provider
-    domain="dev-0hgk5al8.us.auth0.com"
-    clientId="YuzmhPxxiiv5WxGH7AXqDCnTV2ZPnhtN"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
