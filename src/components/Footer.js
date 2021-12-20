871d50169a82520882fd4259de20c38d6b4a5d01
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-12-20 17:15:25 +0300

    feat: add footer element

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 const Footer = () => {
-  return <h4>footer</h4>
+  return (
+    <Wrapper>
+      <h5>&copy; {new Date().getFullYear(<span>ComfySloth</span>)}</h5>
+      <h5>All rights reserved</h5>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.footer`
